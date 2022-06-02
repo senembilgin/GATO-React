850285981c6d675f3b7ecc9ce6db0d947e666b07
senembilgin <senem.bilgin@ozu.edu.tr>
2022-06-02 18:38:31 +0300

    css and changes

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,96 +1,127 @@
-import React, {useState} from "react";
+import axios from 'axios';
+import React, { useState } from "react";
+import "./css/Form.css";
 
+function Form() {
 
-function Form(){
-    const[name, setName] = useState("");
-    const[email,setEmail] = useState("");
-    const[date, setDate] = useState("");
-    const[password, setPassword] = useState("");
-    const[rpassword, setRpassword] = useState("");
+    const [error, setError] = React.useState("");
+    const [formValue, setformValue] = React.useState({
+        userName: '',
+        email: '',
+        dateOfBirth: '',
+        password: ''
+    });
 
+    const handleSubmit = async (e) => {
+        const loginFormData = new FormData();
+        loginFormData.append("userName", formValue.userName)
+        loginFormData.append("email", formValue.email)
+        loginFormData.append("dateOfBirth", formValue.dateOfBirth)
+        loginFormData.append("password", formValue.password)
 
-    function handleNameChange(event){
-        setName(event.target.value);
+        e.preventDefault();
+
+        try {
+            const response = await axios({
+                method: "post",
+                url: "http://localhost:8080/user/save-user",
+                data: loginFormData
+            });
+            alert("user created succesfully")
+        } catch (error) {
+            setError(error.response.data)
+            console.log(error)
+            alert(error.response.data)
+        }
     }
 
-    function handleEmailChange(event){
-        setEmail(event.target.value);
-    }
-
-    function handleDateChange(event){
-        setDate(event.target.value);
-    }
-
-    function handlePasswordChange(event){
-        setPassword(event.target.value);
-    }
-    function handleRpasswordChange(event){
-        setRpassword(event.target.value);
+    const handleChange = (event) => {
+        setformValue({
+            ...formValue,
+            [event.target.name]: event.target.value
+        });
     }
 
     return (
-        <div>
+        <div col-lg-6 col-md-6 col-sm-12>
             <h2>Pre-apply for an Account</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>
                     Username:
-                <input
-                    onChange = {handleNameChange}
-                    type = "text"
-                    value = {name}
-                    maxLength = {20}
-                    required = "true"
-                />
-                </label>
-                <br></br>
-                <label>
-                E-mail:
-                <input  
-                    onChange = {handleEmailChange}
-                    type = "email"
-                    value={email}
-                    required = "true"
-                />
-                </label>
-                <br></br>
-                <label>
-                Date of Birth:
-                <input  
-                    onChange = {handleDateChange}
-                    type = "date"
-                    value={date}
-                    required = "true"
+                    <input id="name"
+                        name="userName"
+                        onChange={handleChange}
+                        type="text"
+                        value={formValue.userName}
+                        maxLength={20}
+                        required="true"
                     />
                 </label>
                 <br></br>
                 <label>
-                Password:
-                <input  
-                    onChange = {handlePasswordChange}
-                    type = "password"
-                    value={password}
-                    minLength = "8"
-                />
+                    E-mail:
+                    <input id="mail"
+                        name="email"
+                        onChange={handleChange}
+                        type="email"
+                        value={formValue.email}
+                        required="true"
+                    />
                 </label>
                 <br></br>
                 <label>
-                Repeat Password:
-                <input  
-                    onChange = {handleRpasswordChange}
-                    type = "password"
-                    value={rpassword}
-                    minLength = "8"
-                />
+                    Date of Birth:
+                    <input id="birth"
+                        name="dateOfBirth"
+                        onChange={handleChange}
+                        type="date"
+                        value={formValue.dateOfBirth}
+                        required="true"
+                    />
                 </label>
                 <br></br>
-                <input  
-                    type = "submit" 
-                    value="Submit"                 
-                />
+                <label>
+                    Password:
+                    <input id="password"
+                        name="password"
+                        onChange={handleChange}
+                        type="password"
+                        value={formValue.password}
+                        minLength="8"
+                    />
+                </label>
+                <br></br>
+                <label>
+                    Repeat Password:
+                    <input id="rpaddword"
+                        onChange={handleChange}
+                        type="password"
+                        // value={}
+                        minLength="8"
+                    />
+                </label>
+                <br></br>
+                <button id="submit"
+                    type="submit"
+                >
+                    Apply
+                </button>
             </form>
+            <div col-lg-6 col-md-6 col-sm-6>
+                <h1>Pre-apply Benefits</h1>
+                <p id="p2"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
         </div>
     );
 }
 
 export default Form;
+
+
+
+
+
+
 
